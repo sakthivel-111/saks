576242fb539ff09c2687fa5ae3d63e72fe7735ec
sakthivel-111 <166297408+sakthivel-111@users.noreply.github.com>
2025-02-18 21:58:19 +0530

    fix : err into out

--- code.java
+++ code.java
@@ -1,6 +1,6 @@
 class code{
     public static void main(String[] args) {
-        System.err.println("hello");
+        System.out.println("hello");
         System.err.println("And changed");       
     }
 }
